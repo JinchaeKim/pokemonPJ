7652402aabf8b1e46003f3af106ae4310472e5bf
JinChaeKim <rlawlsco88@gmail.com>
2025-02-04 00:20:34 +0900

    Docs : "Mock_Data 추가" / Design : "배경이미지 삭제"

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -1,27 +1,32 @@
 import React from "react";
 import styled from "styled-components";
+import MOCK_DATA from "../../data/pokemonList";
 
 const BgMain = styled.div`
   width: auto;
   margin: 20px 90px;
   background-color: #dddddd;
+  /* background-image: url("https://i.namu.wiki/i/og8wF79k4GZrGDG6hkUl9si686oUjZCFIfa3qQb3EJVk4aWj4gK0YJiNjkF2_sSLewYRsI576AWQwy_HuGDl7Q.webp"); */
   padding: 30px;
   border-radius: 10px;
   border: 1px solid lightgray;
-`;
-
-const BgdImg = styled.img`
-  opacity: 50%;
-  width: 70%;
-  display: flex;
-  margin: auto;
 `;
 
 const PokemonList = () => {
   return (
     <>
       <BgMain>
-        <BgdImg src="https://i.namu.wiki/i/og8wF79k4GZrGDG6hkUl9si686oUjZCFIfa3qQb3EJVk4aWj4gK0YJiNjkF2_sSLewYRsI576AWQwy_HuGDl7Q.webp" />
+        {MOCK_DATA.map((data) => {
+          return (
+            <div key={data.id}>
+              <img src={data.img_url} />
+              <div>{data.korean_name}</div>
+              <p>{data.description}</p>
+              <p></p>
+              <button>추가</button>
+            </div>
+          );
+        })}
       </BgMain>
     </>
   );
