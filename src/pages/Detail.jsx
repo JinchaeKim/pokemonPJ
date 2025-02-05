8caf0004d183375e6a28ad3f53a1fbb8adc12188
JinChaeKim <rlawlsco88@gmail.com>
2025-02-05 15:20:18 +0900

    Feat : Detail-디테일 페이지 구현(Dynamic Route/useParams 사용)

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import MOCK_DATA from "../data/pokemonList";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 const Detail = () => {
-  return <div>Detail</div>;
+  // mock_data 중에 내가 선택한 것
+  const datas = MOCK_DATA;
+  const navigate = useNavigate();
+  const params = useParams(); // Router에서 동적 라우팅으로 정해놓은 값(id)
+  console.log("params", params);
+
+  // 배열 한 개만 꺼내오기
+  const targetCard = datas.filter((data) => {
+    return data.id === Number(params.id);
+  });
+  console.log("targetCard", targetCard);
+  return (
+    <div>
+      {targetCard.map((data) => {
+        return (
+          <div key={data.id}>
+            <img src={data.img_url} />
+            <div>{data.korean_name}</div>
+            <div>Type : {data.types}</div>
+            <div>{data.description}</div>
+            <Link to={`/detail/${data.id}`}>go</Link>
+            <button
+              onClick={() => {
+                navigate("/dex");
+              }}
+            >
+              Home
+            </button>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Detail;
--- src/pages/Dex.jsx
+++ src/pages/Dex.jsx
@@ -7,31 +7,32 @@ const Dex = () => {
   // dashboard 추가 포켓몬 구성 state
   const [myMon, setMyMon] = useState([]);
 
-  // 클릭한 포켓몬 카드 정보 가져오기 -> setMyMon에 입력
+  // 카드 추가로직
   const addMypokeMon = (id) => {
+    // 처음에는 빈 배열이기 때문에 6 이상으로 설정 / early return으로 6개까지만
     if (6 <= myMon.length) {
       alert("최대 6개의 카드만 표시됩니다!");
       return;
     }
 
+    // 객체 하나를 반환하는 find 메서드(filter는 배열)
     const findMon = MOCK_DATA.find((i) => {
-      // filter는 배열을 반환
       return i.id === id;
     });
-    setMyMon([...myMon, findMon]);
 
-    // console.log("myMon", myMon);
-    // 6개 이상 alert 띄우기 위치
-    // 처음에는 빈 배열이기 때문에 6 이상으로 설정
+    // 중복 방지 로직
+    if (myMon.some((monEl) => findMon.name === monEl.name)) {
+      alert("이미 추가된 포켓몬입니다.");
+    } else {
+      setMyMon([...myMon, findMon]);
+    }
   };
-  console.log("myMon", myMon);
 
-  // 삭제로직
+  // 카드 삭제로직
   const deleteMypokeMon = (id) => {
     const filterMon = myMon.filter((i) => {
       return i.id !== id;
     });
-    console.log("filterMon", filterMon);
     setMyMon(filterMon);
   };
 
