ad5a1d642515d1ba3fdc78f78da7e1265bb74b90
JinChaeKim <rlawlsco88@gmail.com>
2025-02-05 22:21:39 +0900

    Detail 내의 추가 버튼 구현 진행 중

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import MOCK_DATA from "../data/pokemonList";
 import { Link, useNavigate, useSearchParams } from "react-router-dom";
 import {
@@ -9,8 +9,11 @@ import {
   DTitle,
   DType,
 } from "../styles/StyledComponents";
+import { PokemonContext } from "../context/PokemonContext";
 
 const Detail = () => {
+  const { myMon, addMypokeMon, deleteMypokeMon } = useContext(PokemonContext);
+
   // mock_data 중에 내가 선택한 것
   const datas = MOCK_DATA;
   const navigate = useNavigate();
@@ -22,21 +25,26 @@ const Detail = () => {
   console.log("getId", getId);
 
   // 배열 한 개만 꺼내오기 (map을 위해 filter 사용)
-  const targetCard = datas.filter((data) => {
+  const d_TargetCard = datas.filter((data) => {
     return data.id === Number(getId); // type 일치시키기
   });
-  console.log("targetCard", targetCard); // 클릭한 카드의 정보를 배열로 출력
+  console.log("targetCard", d_TargetCard); // 클릭한 카드의 정보를 배열로 출력
 
   // const setSortParams = (e) => {
   //   searchParams.set("id", "1");
   //   setSearchParams(searchParams);
   // };
 
+  // detail의 id와 myMon의 아이디랑 같다면 true
+  const findD_Card = myMon.find((myMonEl) => {
+    return myMonEl.id === d_TargetCard.id;
+  });
+
   return (
     <DetailMain>
       <DetailBgImg src="https://e1.pxfuel.com/desktop-wallpaper/302/432/desktop-wallpaper-pokemon-scenery-pokemon-landscape.jpg" />
       <DetailCardContainer>
-        {targetCard.map((data) => {
+        {d_TargetCard.map((data) => {
           return (
             <div key={data.id}>
               <img src={data.img_url} />
@@ -54,7 +62,23 @@ const Detail = () => {
               >
                 Home
               </DBtn>
-              {/* <button onClick={()=> {}}>추가</button> */}
+              {findD_Card === true ? (
+                <button
+                  onClick={() => {
+                    deleteMypokeMon(data.id);
+                  }}
+                >
+                  삭제
+                </button>
+              ) : (
+                <button
+                  onClick={() => {
+                    addMypokeMon(data.id);
+                  }}
+                >
+                  추가
+                </button>
+              )}
             </div>
           );
         })}
