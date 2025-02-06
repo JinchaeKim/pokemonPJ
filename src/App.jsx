8c49c9f5e5dfefa2c8cc63d34473b8a1a5c69140
JinChaeKim <rlawlsco88@gmail.com>
2025-02-06 11:41:01 +0900

    Docs : App.jsx store 주입

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import Router from "./shared/Router";
 import styled, { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 import { PokemonProvider } from "./context/PokemonContext";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 const GlobalStyle = createGlobalStyle`${reset}`;
 
 const App = () => {
   return (
-    <PokemonProvider>
+    <Provider store={store}>
       <GlobalStyle />
       <Router />
-    </PokemonProvider>
+    </Provider>
   );
 };
 
