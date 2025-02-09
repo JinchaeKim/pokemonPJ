54b41d4b4d2db51a4984429aeeb5cb49765d888b
JinChaeKim <rlawlsco88@gmail.com>
2025-02-09 14:44:30 +0900

    Fix : vercel 새로고침 오류 수정1 - route path="/*" 추가 & import 정리

--- src/pages/Dex.jsx
+++ src/pages/Dex.jsx
@@ -1,7 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 import Dashboard from "../components/Dashboard/Dashboard";
 import PokemonList from "../components/PokemonList/PokemonList";
-import MOCK_DATA from "../data/pokemonList";
 
 const Dex = () => {
   return (
